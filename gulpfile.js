38dbe30822f1ca6c3b8db9f27cb3b85c08fbbb44
Slava Krivtsov <slava.kr.89@gmail.com>
2016-07-21 15:47:07 +0300

    add(gulp-copy-task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// gulp.task('default', function() {
+//   // place code for your default task here
+// });
+
+gulp.task('copy', function () {
+    gulp.src('./client/index.html')
+        .pipe(gulp.dest('./public/'));
 });
+
+gulp.task('default', ['copy']);
