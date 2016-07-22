6799afbee533c5745da1334ffbd2408218ec86c4
JackLip <ev.lipskiy@gmail.com>
2016-07-22 10:39:49 +0300

    add static

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + './public'));
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
 
