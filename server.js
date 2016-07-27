d48a86757d2268d5015e97c57626556bb8998f6c
JackLip <ev.lipskiy@gmail.com>
2016-07-27 12:51:04 +0300

    add mongoose

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+mongoose.connect('process.env.MONGO_DB');
 
 app.use(express.static(__dirname + '/client'));
 
