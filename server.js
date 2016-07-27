a2cffcb627d6ea1ac3b6d82d6906b810a8155b7b
JackLip <ev.lipskiy@gmail.com>
2016-07-27 13:33:17 +0300

    close #22

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + '/client'));
 /*app.get('/', function (req, res) {
   res.send('Hello World!');
 });*/
-
 app.listen(port, function () {
   console.log('Example app listening ' + port);
 });
