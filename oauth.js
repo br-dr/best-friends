4f2434a691c991c1bd8788ea0fe9a91ee1b5e324
Slava Krivtsov <slava.kr.89@gmail.com>
2016-07-30 02:43:28 +0300

    add(google-auth)

---
+++ oauth.js
@@ -0,0 +1,10 @@
+var ids = {
+  google: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  }
+  
+};
+
+module.exports = ids;
--- server.js
+++ server.js
@@ -13,3 +13,123 @@ app.use(express.static(__dirname + '/client'));
 app.listen(port, function () {
   console.log('Example app listening ' + port);
 });
+// var config = require('./oauth.js');
+
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+// app.use(express.cookieParser());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var session = require('express-session');
+app.use(passport.initialize())
+app.use(passport.session()); 
+
+
+// var config = require('./client/configs/oauth.js');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+var dotenv = require('dotenv');
+dotenv.config();
+
+// create a user model
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    oauthID: String,
+    name: String,
+    created: Date,
+    firstName: String,
+    lastName: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+
+
+// config
+
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.use(new GoogleStrategy({
+  // clientID: config.google.clientID,
+  // clientSecret: config.google.clientSecret,
+  // callbackURL: config.google.callbackURL
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    User.findOne({ oauthID: profile.id }, function (err, user) {
+      if (err) {
+        console.log(err);
+      }
+      if (!err && user !== null) {
+        done(null, user);
+      } else {
+        user = new User({
+          oauthID: profile.id,
+          name: profile.displayName,
+          created: Date.now()
+        });
+        user.save(function (err) {
+          if (err) {
+            console.log(err);  // handle errors!
+          } else {
+            console.log("saving user ...");
+            done(null, user);
+          }
+        });
+      }
+    });
+  }
+));
+
+// serialize and deserialize
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+
+//routes
+
+app.get('/profile', ensureAuthenticated, function(req, res){
+  User.findById(req.session.passport.user, function(err, user) {
+    if(err) {
+      console.log(err);  // handle errors
+    } else {
+      res.render('profile', { user: user});
+    }
+  });
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read'
+  ] }
+));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/profile');
+  });
+
+  app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
+
+
