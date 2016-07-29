b704b2da8e9a3d7a3a4064eb2cc2d740a4ce8838
Slava Krivtsov <slava.kr.89@gmail.com>
2016-07-29 12:35:44 +0300

    add(basic ui-routing without auth and passport yet

---
+++ client/configs/states.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+    
+    angular
+        .module('app')
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state('home', {
+                    url: "/",
+                    templateUrl: "components/home/home.html"   
+                }) 
+                .state('login', {
+                    url: "/login",
+                    templateUrl: "components/login/login.html"                    
+                })
+                .state('registration', {
+                    url: "/registration",
+                    templateUrl: "components/registration/registration.html"                    
+                })
+                .state('profile', {
+                    url: "/profile",
+                    templateUrl: "components/profile/profile.html"   
+                })
+                                            
+               
+        });
+
+
+})();
