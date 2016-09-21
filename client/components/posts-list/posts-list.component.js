b5866cae7c189036afa36f61d5efaae7c8f89f6e
Slava Krivtsov <slava.kr.89@gmail.com>
2016-09-21 22:12:16 +0300

    close #60 add (restrict posting to user who not followed by post owner)

--- client/components/posts-list/posts-list.component.js
+++ client/components/posts-list/posts-list.component.js
@@ -12,9 +12,9 @@
             }
         });
 
-    PostsListController.$inject = ['$http'];
+    PostsListController.$inject = ['$http', 'toastr'];
 
-    function PostsListController($http) {
+    function PostsListController($http, toastr) {
         var vm = this;
 
         angular.extend(vm, {
@@ -25,7 +25,7 @@
             },
             cancelPostInput: cancelPostInput,
             addPost: addPost,
-            deletePost: deletePost
+            deletePost: deletePost,
         });
 
         function cancelPostInput() {
@@ -40,7 +40,8 @@
                     vm.posts.push(response.data);
                     vm.cancelPostInput();
                 }).catch(function() {
-                    console.log('Failed');
+                    // console.log('Failed');
+                    toastr.error('This user is not following you...', 'You can\'t post here!');
                 });
         }
 
